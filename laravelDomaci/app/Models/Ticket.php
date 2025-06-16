e34c0f9cebe4e1cc600f3a3276659ad2f1445865
jovanastajcic <jovanastaji@yahoo.com>
2025-06-16 21:25:52 +0200

    Finalizovanje modela

--- laravelDomaci/app/Models/Ticket.php
+++ laravelDomaci/app/Models/Ticket.php
@@ -6,5 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'event_id',
+        'purchase_date',
+        'ticket_code',
+        'status'
+    ];
+
+    protected $casts = [
+        'purchase_date' => 'datetime'
+    ];
+
+    // Relacije
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
+    }
+
+    // Generisanje jedinstvenog koda ulaznice
+    public static function generateTicketCode()
+    {
+        do {
+            $code = 'TKT-' . strtoupper(uniqid());
+        } while (self::where('ticket_code', $code)->exists());
+
+        return $code;
+    }
 }
